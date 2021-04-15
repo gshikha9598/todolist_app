0b0daf196e2797c83c111f6aff2cdf88b8797ecf
pratik <pratik.c@morphedo.com>
2021-04-15 13:48:57 +0530

    list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [add, addUpdate] = useState({ title: '' })
+  const [data, dataUpdate] = useState([{ id: '1', title: 'todo 1', desc: 'desc1', isDone: false },
+  { id: '2', title: 'todo 2', desc: 'desc1df', isDone: false },
+  { id: '3', title: 'todo 3', desc: 'desc1df', isDone: false }])
+  return (<>
+    <input type="text"
+    className='add-input'
+      value={add.title}
+      onChange={(e) => { addUpdate({ ...add, title: e.target.value }) }}
+    />
+    <button 
+    className="add-btn"
+     onClick={() => { dataUpdate([...data, add]) }}>
+        ADD </button>
+    <div className="">
+      {
+        data.length > 0 &&
+        data.map((i, index) => {
+          return <div className="title-div">
+            {i.title}
+          </div>
+        })
+      }
     </div>
-  );
+  </>);
 }
 
 export default App;
